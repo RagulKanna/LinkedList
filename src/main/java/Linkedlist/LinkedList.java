3f8576059c92ade862d539cd827c9281a783238c
RagulKanna <ragulragi619@gmail.com>
2021-09-10 14:02:39 +0530

    Added:Inserting an element at specific place in linkedlist

--- src/main/java/Linkedlist/LinkedList.java
+++ src/main/java/Linkedlist/LinkedList.java
@@ -75,6 +75,29 @@ public class LinkedList {
 
     }
 
+    public void delete(int delete){
+        int size=0;
+        Node temp = head;
+        while (temp != null){
+            temp = temp.next;
+            size++;
+        }
+        temp=head;
+        while(temp.next.data != delete){
+            temp = temp.next;
+        }
+        System.out.println("\nthe size of the list is "+size);
+        temp.next = temp.next.next;
+        temp.next.next=null;
+        temp = head;
+        size=0;
+        while (temp != null){
+            temp = temp.next;
+            size++;
+        }
+        System.out.println("\nthe size of the list is "+size);
+    }
+
 
     public void display(){
         if (head == null) {
